828df21b8f545c0c8716d3e51f69a79fb1656fcb
Bogdan Bărbulescu <130338564+bogdanbarbulescu@users.noreply.github.com>
2025-01-23 17:27:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 
+const images = document.querySelectorAll('.gallery img');
+const lightbox = document.querySelector('.lightbox');
+
+images.forEach(image => {
+  image.addEventListener('click', () => {
+    lightbox.style.display = 'block';
+    lightbox.src = image.src;
+  });
+});
+
+lightbox.addEventListener('click', () => {
+  lightbox.style.display = 'none';
+});
